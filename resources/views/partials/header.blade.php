184a0c24fa5b9abdc50c92b37a8fcdc31d9ae33c
MattiaFezzardi <mattiafezzardi1993@gmail.com>
2023-05-02 19:53:59 +0200

    aggiunto parziali scss

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,23 @@
-<header>header
-    
+<header>
+    <div class="bg">
+        <div class="container">
+            <div>
+                <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="logo">
+                <!-- <h1>ff</h1> -->
+            </div>
+
+            <ul>
+                <li>CHARACTERS</li>
+                <li>COMICS</li>
+                <li>MOVIES</li>
+                <li>TV</li>
+                <li>GAMES</li>
+                <li>COLLECTIBLES</li>
+                <li>VIDEOS</li>
+                <li>FANS</li>
+                <li>NEWS</li>
+                <li>SHOP</li>
+            </ul>
+        </div>
+    </div>    
 </header>
