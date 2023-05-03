5a864df347ead6f531944a364fa42014e5c72285
MattiaFezzardi <mattiafezzardi1993@gmail.com>
2023-05-03 14:12:38 +0200

    inserite cards con imgs mancanti

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,11 +9,14 @@
     <div class="container">
         <h3>CURRENT SERIES</h3>
         <ul>
+            @foreach ($fumetti as $fumetto)
             <li>
                 <div class="box">
-                    
+                    <img src="{{ $fumetto['thumb'] }}" alt="Immagine">
+                    <div class="serie">{{ $fumetto['title'] }}</div>
                 </div>
             </li>
+            @endforeach
         </ul>
         <button>LOAD MORE</button>
     </div>
